a537d9291181e05b6a68834fc7bcfa9240c0451c
Max Thorn <mthorn.dev@outlook.com>
2025-11-22 01:20:12 +0100

    move frontend folder

---
+++ frontend/src/app/page.tsx
@@ -0,0 +1,13 @@
+import styles from "./page.module.css";
+
+const Home = () => {
+  return (
+    <>
+      <h1 className={styles.ibm}>Homepage</h1>
+      <h1 className={styles.crimson}>Homepage</h1>
+      <h1 className={styles.jetbrain}>Homepage</h1>
+    </>
+  );
+};
+
+export default Home;
--- src/app/page.tsx
+++
@@ -1,7 +0,0 @@
-// import styles from "./page.module.css";
-
-const Home = () => {
-  return <div>Homepage</div>;
-};
-
-export default Home;
